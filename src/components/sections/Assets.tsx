2da79db9c2495cf9f56cd539acc7f5474c21255d
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2024-06-14 00:06:36 -0500

    Assets cards styled

--- src/components/sections/Assets.tsx
+++ src/components/sections/Assets.tsx
@@ -11,11 +11,23 @@ interface AssetCardProps {
   };
 }
 
-function AssetCard({asset}: AssetCardProps) {
+function AssetCard({ asset }: AssetCardProps) {
   return (
     <a href={asset.link} target='_blank'>
-      <img src={asset.img} alt={asset.name} className='h-24 w-24 object-cover' />
-      {asset.name}
+      <div className='flex border-zinc-400/10 border-[1.5px] backdrop-blur-md bg-slate-800/80
+                      rounded-3xl overflow-hidden p-4 space-x-4 hover:bg-slate-700/80
+                      hover:scale-[1.03] transition duration-300 ease-in-out group'>
+        <img src={asset.img} alt={asset.name} className='h-24 w-24 object-cover rounded-full' />
+        <div className='text-left w-[30rem]'>
+          <h1 className='font-semibold text-xl'>
+            {asset.name}
+          </h1>
+          <p className='opacity-80'>
+            {asset.description}
+          </p>
+        </div>
+      </div>
+
     </a>
   )
 }
@@ -26,7 +38,7 @@ function AssetCard({asset}: AssetCardProps) {
 
 export default function Assets() {
   return (
-    <div className='flex flex-column'>
+    <div className='flex flex-col space-y-4'>
       {
         assetsList.map((asset) => (
           <AssetCard asset={asset} key={asset.name} />
